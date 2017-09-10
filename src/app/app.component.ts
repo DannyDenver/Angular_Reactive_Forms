f309970e39ecf0d294b448746a805df15f361c09
Daniel Taylor <Daniel Taylor>
2017-09-10 09:26:20 -0600

    Custom FormControl validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +8,35 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
-
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
 ngOnInit(){
     this.signupForm = new FormGroup({
-        'username': new FormControl('DanMan'),
-        'email': new FormControl(null),
-        'gender': new FormControl('male')
+        'userData': new FormGroup({ 
+          'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]), //validators  this.forbiddenNames/// will not be in same class when Angular checks so broke 
+        'email': new FormControl(null, [Validators.required, Validators.email])
+        }),        
+        'gender': new FormControl('male'),
+        'hobbies': new FormArray([])
     })
 }
+
+onSubmit(){
+    console.log(this.signupForm);
+}
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+
+  forbiddenNames(control: FormControl):{ [s:string]: boolean } {//creating new validator function    
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){   // if value not in array returns -1 which is interpreted as true 
+      return { 'nameIsForbidden': true}; // only pass object if true 
+    }
+    return null; // form control valid 
+  
+  }
 
 }
